19b07c4f81808e55aca462d60b63a54a480a881a
MirkusX <marcusk.kodehode@gmail.com>
2023-03-15 14:44:40 +0100

    added paragraph

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,7 +7,13 @@ export const Card = () => {
         <div>
           <img src={hero} />
         </div>
-        <div></div>
+        <div>
+          <h1>Order Summary</h1>
+          <p>
+            You can now listen to millions of songs, audiobooks, and podcasts on
+            any device anywhere you like
+          </p>
+        </div>
       </div>
     </section>
   );
